44adbb1973fa17214072ad536c61987341ccb434
kimyb8870 <qhd0084@gmail.com>
2021-10-05 03:07:52 +0900

    Add: redux module user.js

---
+++ src/modules/user.js
@@ -0,0 +1,46 @@
+import { takeLatest } from '@redux-saga/core/effects';
+import { createAction, handleActions } from 'redux-actions';
+import * as authAPI from '../lib/api/client';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/api/createRequestSaga';
+
+const TEMP_SET_USER = 'user/TEMP_SET_USER';
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
+  createRequestActionTypes('user/CHECK');
+
+export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
+export const check = createAction(CHECK);
+
+const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+export function* userSaga() {
+  yield takeLatest(CHECK, checkSaga);
+}
+
+const initialState = {
+  user: null,
+  checkError: null,
+};
+
+const user = handleActions(
+  {
+    [TEMP_SET_USER]: (state, { payload: user }) => ({
+      ...state,
+      user,
+    }),
+    [CHECK_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      user,
+      checkError: null,
+    }),
+    [CHECK_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      user: null,
+      checkError: true,
+    }),
+  },
+  initialState,
+);
+
+export default user;
