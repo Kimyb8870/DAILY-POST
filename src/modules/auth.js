780630670d554f2f13f088a8302c0fbbf9947895
kimyb8870 <qhd0084@gmail.com>
2021-10-05 06:25:28 +0900

    Fix: redux module auth.js
    login action parameter error fixed

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -31,7 +31,7 @@ export const register = createAction(REGISTER, ({ username, password }) => ({
   password,
 }));
 
-export const login = createAction(LOGIN, (username, password) => ({
+export const login = createAction(LOGIN, ({ username, password }) => ({
   username,
   password,
 }));
