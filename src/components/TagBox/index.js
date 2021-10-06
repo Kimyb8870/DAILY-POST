a839881668bea3c42d3b00a70bd90c4a53a938a1
kimyb8870 <qhd0084@gmail.com>
2021-10-06 19:22:55 +0900

    Update: TagBox component
    state
    1. input -> tag text
    2. localTags -> tag array

--- src/components/TagBox/index.js
+++ src/components/TagBox/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   StyledTagBox,
   StyledTagForm,
@@ -6,25 +6,65 @@ import {
   StyledTagItem,
 } from './styles/TagBox';
 
-const TagItem = React.memo(({ tag }) => <StyledTagItem>#{tag}</StyledTagItem>);
+const TagItem = React.memo(({ tag, onDelete }) => (
+  <StyledTagItem onClick={() => onDelete(tag)}>#{tag}</StyledTagItem>
+));
 
-const TagList = React.memo(({ tags }) => (
+const TagList = React.memo(({ tags, onDelete }) => (
   <StyledTagList>
     {tags.map((tag) => (
-      <TagItem key={tag} tag={tag} />
+      <TagItem key={tag} tag={tag} onDelete={onDelete} />
     ))}
   </StyledTagList>
 ));
 
 const TagBox = () => {
+  const [input, setInput] = useState('');
+  const [localTags, setLocalTags] = useState([]);
+
+  const insertTag = useCallback(
+    (tag) => {
+      if (!tag) return;
+      if (localTags.includes(tag)) return;
+      setLocalTags([...localTags, tag]);
+    },
+    [localTags],
+  );
+
+  const deleteTag = useCallback(
+    (tag) => {
+      if (!tag) return;
+      if (!localTags.includes(tag)) return;
+      setLocalTags(localTags.filter((t) => t !== tag));
+    },
+    [localTags],
+  );
+
+  const onChange = useCallback((e) => {
+    setInput(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      insertTag(input.trim());
+      setInput('');
+    },
+    [input, setInput, insertTag],
+  );
+
   return (
     <StyledTagBox>
       <h4>태그</h4>
-      <StyledTagForm>
-        <input placeholder="태그를 입력하세요" />
+      <StyledTagForm onSubmit={onSubmit}>
+        <input
+          placeholder="태그를 입력하세요"
+          onChange={onChange}
+          value={input}
+        />
         <button type="submit">추가하기</button>
       </StyledTagForm>
-      <TagList tags={['태그1', '태그2', '태그3']} />
+      <TagList tags={localTags} onDelete={deleteTag} />
     </StyledTagBox>
   );
 };
