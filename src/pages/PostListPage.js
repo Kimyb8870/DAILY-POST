bb84aff8f2d238f66251ccb227e049fecfe93658
kimyb8870 <qhd0084@gmail.com>
2021-10-09 11:53:07 +0900

    Feature: PostListPage
    PostList, Pagination

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../containers/HeaderContainer';
+import PaginationContainer from '../containers/PaginationContainer';
+import PostListContainer from '../containers/PostListContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <div>
-        Non sit duis deserunt elit officia occaecat irure quis Lorem ullamco
-        sunt. Nulla deserunt occaecat est consectetur id aute mollit aliqua
-        laborum dolore sit officia aliquip. Excepteur commodo amet proident
-        culpa sunt dolore ea eu commodo commodo cillum. Pariatur cillum in
-        commodo sunt in id excepteur elit. Elit id incididunt dolor irure
-        aliquip cillum laboris non dolore voluptate velit sunt.
-      </div>
+      <PostListContainer />
+      <PaginationContainer />
     </>
   );
 };
